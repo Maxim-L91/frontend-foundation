988e807517dacf7106d4203ece00e648251b8ab1
Maxim-L91 <maximrus62@gmail.com>
2026-04-24 22:33:15 +0300

    config: setup project structure (root, base, out, public)

--- minista.config.js
+++ minista.config.js
@@ -2,4 +2,17 @@ import { defineConfig, pluginSsg, pluginBundle, pluginSprite } from "minista"
 import svgoConfig from "./svgo.config.mjs"
 import path from 'path'
 
-export default defineConfig()
+export default defineConfig({
+  // Корень проекта (обычно не меняется)
+  root: '', // В данном случае это папка, где лежит minista.config.js
+
+  // Базовый путь (если деплой не в корень сайта, поменяй)
+  // префикс, который будет добавляться ко всем путям в продакшене: Скрипты, стили, картинки
+  base: '/',
+
+  // Папка с публичными файлами (копируются как есть без обработки бандлером)
+  public: 'public',
+
+  // Папка для сборки (туда складывается результат)
+  out: 'dist',
+})
