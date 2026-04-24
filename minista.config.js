8147a5f950cd264e468961e70bb1d8373001df43
Maxim-L91 <maximrus62@gmail.com>
2026-04-24 22:30:48 +0300

    chore: init minista config

--- minista.config.js
+++ minista.config.js
@@ -1,5 +1,5 @@
-import { defineConfig, pluginSsg } from "minista"
+import { defineConfig, pluginSsg, pluginBundle, pluginSprite } from "minista"
+import svgoConfig from "./svgo.config.mjs"
+import path from 'path'
 
-export default defineConfig({
-  plugins: [pluginSsg()],
-})
+export default defineConfig()
